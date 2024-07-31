8e48aae2197be8283fef5ec3eb47959d842e7e0e
David Nanjila <nanjiladavid2@gmail.com>
2024-07-31 16:18:02 +0000

    Create amenity class that inherits from the basemodel
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

---
+++ models/engine/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Amenity class that inherits from the BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class Amenity name = """
+    name = ""
